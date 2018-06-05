456f668e6fe0f3967ebf52259ecde3bbe1e6e316
MiguelDelgado27 <miguel.9dc@gmail.com>
2018-06-05 12:58:11 -0500

    Subo la estructura del proyecto inicializado con spring boot

---
+++ src/main/java/com/ws/WorkSwapApplication.java
@@ -0,0 +1,12 @@
+package com.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkSwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkSwapApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ws/model/entity/Usuario.java
@@ -0,0 +1,5 @@
+package com.ws.model.entity;
+
+public class Usuario {
+
+}
